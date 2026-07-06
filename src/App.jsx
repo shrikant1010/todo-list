8815bd1084a8880bbd5efb8fff2dd11fa1731b79
shrikant1010 <shrikantbagal09@gmail.com>
2026-07-06 21:26:24 +0530

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Content from "./Content";
-import Footer from "./Footer";
 const App = () => {
   const [mode, setMode] = useState(true);
   function changeMode(value) {
@@ -11,7 +10,6 @@ const App = () => {
     <div className={`card ${mode === "dark" ? "dark" : ""}`}>
       <Header changeMode={changeMode} />
       <Content />
-      <Footer />
     </div>
   );
 };
